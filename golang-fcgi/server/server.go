c185b128c1faa9aaf16fddd3b325c467c7d3b3eb
Peter Beard <peter.beard@uvu.edu>
2025-11-12 17:15:52 -0700

    Simple RSS style feed through SSI

--- golang-fcgi/server/server.go
+++ golang-fcgi/server/server.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
 	"log/slog"
 	"net"
 	"net/http"
 	"net/http/fcgi"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -20,15 +24,9 @@ func main() {
 
 	router := http.NewServeMux()
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_SERVER := fcgi.ProcessEnv(r)
+	router.HandleFunc("GET /blog-feed.go", RssFeed("blog"))
 
-		data, err := json.Marshal(_SERVER)
-		if err != nil {
-			panic(err)
-		}
-		w.Write(data)
-	})
+	router.HandleFunc("GET /event-feed.go", RssFeed("event"))
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:9000",
@@ -41,5 +39,78 @@ func main() {
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
+	}
+}
+
+type Post struct {
+	UserId float64 `json:"userId"`
+	PostId float64 `json:"id"`
+	Title  string  `json:"title"`
+	Body   string  `json:"body"`
+}
+
+var cardTemplate = `
+			<ul>
+				{{range $i, $post := .}}
+					{{ with $post }}
+					 	<li>
+							<article class="card card-%s">
+								<header>{{ .Title }}</header>
+								 <img src="https://picsum.photos/69{{- $i -}}/40{{- $i -}}?random=%s" />
+								 <p class="description">{{ .Body }}</p>
+							 </article>
+						</li>
+					{{ end }}
+				{{end}}
+			</ul>
+		`
+
+func RssFeed(feedType string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		response, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer response.Body.Close()
+
+		jsonData, err := io.ReadAll(response.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var data []map[string]any
+		err = json.Unmarshal(jsonData, &data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		limit, err := strconv.Atoi(r.URL.Query().Get("limit"))
+		if err != nil {
+			limit = 5
+		}
+
+		posts := []Post{}
+
+		for i := range limit {
+			post := Post{
+				UserId: data[i]["userId"].(float64),
+				PostId: data[i]["id"].(float64),
+				Title:  data[i]["title"].(string),
+				Body:   data[i]["body"].(string),
+			}
+
+			posts = append(posts, post)
+		}
+
+		template, err := template.New("RssFeed").Parse(fmt.Sprintf(cardTemplate, feedType, feedType))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		template.Execute(w, &posts)
 	}
 }
